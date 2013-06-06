385f617c0a3b4233605f27f0e078242267bb68ee
Sébastien Drouyer <sdrdis@gmail.com>
2013-06-06 10:25:49 +0200

    Some typo fix
    You don't need " since you are using <<EOF.

--- osm/classes/controller/front.ctrl.php
+++ osm/classes/controller/front.ctrl.php
@@ -23,7 +23,7 @@ class Controller_Front extends Controller_Front_Application
     {
         extract($enhancer_args,EXTR_SKIP);
          return <<<EOF
-<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=$left,$bottom,$right,$top&amp;layer=mapnik" style="border: 1px solid black"></iframe><br /><small><a href="http://www.openstreetmap.org/?lat=$lat&amp;lon=$lon&amp;zoom=$zoom&amp;layers=M">View Larger Map</a></small>"
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=$left,$bottom,$right,$top&amp;layer=mapnik" style="border: 1px solid black"></iframe><br /><small><a href="http://www.openstreetmap.org/?lat=$lat&amp;lon=$lon&amp;zoom=$zoom&amp;layers=M">View Larger Map</a></small>
 EOF;
     }
 
